57ab0da595dc7b1232d6b2bff44108a41fc58cb9
Madhavarapu Renusree <renusreemadhavarapu007@gmail.com>
2022-11-16 23:10:47 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let clickBtn = document.getElementById("button")
+let canvas = document.getElementById("canvas")
+clickBtn.addEventListener("click", () => {
+
+    canvas.style.backgroundColor= 'rgb('+ Math.round(Math.random()*255)+','+ Math.round(Math.random()*255)+','+ Math.round(Math.random()*255)+')'
+
+
+})
+
+
+
+
+
